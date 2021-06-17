91a285c7b816a80414a07c15854dceca9b724528
whhxd <1757280079@qq.com>
2021-06-17 23:00:16 +0800

    account api

--- src/main/java/com/example/health/controller/AccountController.java
+++ src/main/java/com/example/health/controller/AccountController.java
@@ -1,21 +1,44 @@
 package com.example.health.controller;
 
+import com.example.health.data.AccountBean;
+import com.example.health.data.ApiResult;
+import com.example.health.data.group.BasicAccountInfo;
+import com.example.health.data.group.ChangePasswordInfo;
 import com.example.health.repository.AccountRepository;
+import com.example.health.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
+@RequestMapping("account")
 public class AccountController {
 
     @Autowired
-    private AccountRepository accountRepository;
+    private AccountService accountService;
 
-    @PostMapping(path = "login")
-    void login(@Param("username") String username, @Param("password") String password) {
+    @PostMapping(path = "signIn")
+    ApiResult<String> signIn(@Validated(BasicAccountInfo.class) AccountBean accountBean,
+            HttpSession session) {
+        return accountService.signIn(accountBean, session);
+    }
 
+    @PostMapping(path = "signUp")
+    ApiResult<String> signUp(@Validated(BasicAccountInfo.class) AccountBean accountBean) {
+        return accountService.signUp(accountBean);
+    }
+
+    @PostMapping(path = "changePassword")
+    ApiResult<String> changePassword(@Validated(ChangePasswordInfo.class) AccountBean accountBean,
+                                     HttpSession session) {
+        return accountService.changePassword(accountBean, session);
     }
 
 }
--- src/main/java/com/example/health/data/AccountBean.java
+++ src/main/java/com/example/health/data/AccountBean.java
@@ -1,11 +1,26 @@
 package com.example.health.data;
 
+import com.example.health.data.group.BasicAccountInfo;
+import com.example.health.data.group.ChangePasswordInfo;
 import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 public class AccountBean {
+
+    @NotNull(groups = BasicAccountInfo.class)
     String username;
+
+    @NotNull(groups = {BasicAccountInfo.class, ChangePasswordInfo.class})
+    @Size(min = 4, max = 16, groups = {BasicAccountInfo.class, ChangePasswordInfo.class})
     String password;
+
+    @NotNull(groups = ChangePasswordInfo.class)
+    @Size(min = 4, max = 16, groups = ChangePasswordInfo.class)
     String newPassword;
+
+    @NotNull(groups = BasicAccountInfo.class)
     int role;
 }
